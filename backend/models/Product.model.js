a8f380effc98f5c43e920f0b5f310b63e1b1a631
Sonu-7891 <sonuverma789163@gmail.com>
2024-11-30 23:09:19 +0530

    code ok

--- backend/models/Product.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  pricePerUnit: { type: Number, required: true },
-  image: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Product", ProductSchema);
